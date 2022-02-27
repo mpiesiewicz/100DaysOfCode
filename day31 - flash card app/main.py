dcaad629963b0f4f1d0fbee38a6c32ec4aea6e35
mpiesiewicz <michal.piesiewicz@gmail.com>
2022-02-27 20:15:58 +0100

    added menu, language change option and spanish language support

--- day31 - flash card app/main.py
+++ day31 - flash card app/main.py
@@ -1,20 +1,27 @@
 import random
 import tkinter as tk
 import tkinter.messagebox
-
+import tkinter.simpledialog
 from PIL import Image, ImageTk
 import pandas as pd
-from random import randint, choice
-import time
-import functools
-
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_WORD = ('Ariel', 20, 'bold')
 FONT_LANGUAGE = ('Ariel', 10, 'italic')
+FONT_POPULARITY = ('Ariel', 10, 'italic')
 SEEN_CARDS = list()
 current_card = dict()
 current_card_index = int()
+words_to_learn = str()
+data_file = str()
+
+with open('settings.txt') as settings_file:
+    LANGUAGE = settings_file.read()
+
+if LANGUAGE == 'FR':
+    language_flag = 'French'
+else:
+    language_flag = 'Spanish'
 
 
 def cards_resize(img, width, height):
@@ -25,16 +32,33 @@ def cards_resize(img, width, height):
 
 
 def data_initialize():
+    global language_flag, words_to_learn, data_file, to_learn
+
+    if LANGUAGE == 'FR':
+        language_flag = 'French'
+    else:
+        language_flag = 'Spanish'
+
+    data_file = 'data/' + LANGUAGE + '_words.csv'
+    words_to_learn = 'data/' + LANGUAGE + '_words_to_learn.csv'
+
     try:
-        data = pd.read_csv('data/words_to_learn.csv')
+        data = pd.read_csv(words_to_learn)
     except pd.errors.EmptyDataError:
-        data = pd.read_csv('data/french_words.csv')
+        data = pd.read_csv(data_file)
     to_learn = data.to_dict(orient='records')
     return to_learn
 
 
-def not_known():
-    global current_card, current_card_index
+def reset():
+    global to_learn
+    rows = 9000
+    while rows >= 8001:
+        rows = tkinter.simpledialog.askinteger('Resetting...', 'How many words do you want to know?\n'
+                                                              'Max 8000')
+    data = pd.read_csv(data_file, nrows=rows)
+    data.to_csv(words_to_learn, index=False)
+    to_learn = data.to_dict(orient='records')
     next_card()
 
 
@@ -42,14 +66,15 @@ def known():
     global current_card_index
     to_learn.pop(current_card_index)
     if len(to_learn) == 0:
-        tkinter.messagebox.showinfo(title='CONGRATS!', message='You know all the words.')
-        on_closing()
+        tkinter.messagebox.showinfo(title='CONGRATS!', message='You know all the words!\n'
+                                                               'Impressive. Resetting...')
+        reset()
     else:
         next_card()
 
 
 def next_card():
-    global flip_timer, current_card, current_card_index
+    global flip_timer, current_card, current_card_index, LANGUAGE, language_flag
 
     # prevent drawing the same index again
     old_index = current_card_index
@@ -61,8 +86,10 @@ def next_card():
 
     window.after_cancel(flip_timer)
     canvas.itemconfig(image, image=card_front)
-    canvas.itemconfig(label_word, text=current_card['French'], fill='black')
-    canvas.itemconfig(label_language, text='French', fill='black')
+    canvas.itemconfig(label_word, text=current_card[LANGUAGE], fill='black')
+    canvas.itemconfig(label_language, text=language_flag, fill='black')
+    canvas.itemconfig(label_popularity, text=current_card['Popularity'], fill='black')
+
     flip_timer = window.after(3000, flip_card)
 
 
@@ -71,13 +98,34 @@ def flip_card():
     canvas.itemconfig(image, image=card_back)
     canvas.itemconfig(label_word, text=current_card['English'], fill='white')
     canvas.itemconfig(label_language, text='English', fill='white')
+    canvas.itemconfig(label_popularity, text=current_card['Popularity'], fill='white')
+
     SEEN_CARDS.append(current_card)
 
 
-def on_closing():
+def language_fr():
+    on_closing(False)
+    global LANGUAGE
+    LANGUAGE = "FR"
+    data_initialize()
+
+
+def language_es():
+    on_closing(False)
+    global LANGUAGE
+    LANGUAGE = "ES"
+    data_initialize()
+
+
+def on_closing(destroy=True):
     saved_data = pd.DataFrame(to_learn)
-    saved_data.to_csv('data/words_to_learn.csv', index=False)
-    window.destroy()
+    saved_data.to_csv(words_to_learn, index=False)
+
+    with open('settings.txt', 'w') as settings_file:
+        settings_file.write(LANGUAGE)
+
+    if destroy:
+        window.destroy()
 
 
 # Window settings
@@ -89,7 +137,6 @@ window.resizable(width=False, height=False)
 
 # Data loading
 to_learn = data_initialize()
-
 
 # ------------------- UI SETTINGS -------------------
 
@@ -104,15 +151,32 @@ canvas = tk.Canvas(width=300, height=150, highlightthickness=0)
 image = canvas.create_image(150, 75, image=card_front)
 label_language = canvas.create_text(150, 40, font=FONT_LANGUAGE, text='sample')
 label_word = canvas.create_text(150, 75, font=FONT_WORD, text='word')
+label_popularity = canvas.create_text(250, 130, font=FONT_POPULARITY, text='1')
 
 btn_right = tk.Button(image=tick_right, highlightthickness=False, borderwidth=0, command=known)
-btn_wrong = tk.Button(image=tick_wrong, highlightthickness=False, borderwidth=0, command=not_known)
+btn_wrong = tk.Button(image=tick_wrong, highlightthickness=False, borderwidth=0, command=next_card)
 
 canvas.grid(column=0, row=0, columnspan=2, rowspan=5)
 btn_wrong.grid(column=0, row=5)
 btn_right.grid(column=1, row=5)
 
-flip_timer = window.after(0, not_known)
+# menubar
+menubar = tk.Menu(window)
+
+filemenu = tk.Menu(menubar, tearoff=0)
+filemenu.add_command(label='Reset', command=reset)
+filemenu.add_separator()
+filemenu.add_command(label='Exit', command=on_closing)
+menubar.add_cascade(label="File", menu=filemenu)
+
+Languages_menu = tk.Menu(menubar, tearoff=0)
+Languages_menu.add_command(label='FR', command=language_fr)
+Languages_menu.add_command(label='ES', command=language_es)
+menubar.add_cascade(label="Languages", menu=Languages_menu)
+
+window.config(menu=menubar)
+
+flip_timer = window.after(0, next_card)
 
 window.protocol("WM_DELETE_WINDOW", on_closing)
 
