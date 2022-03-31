bf1aef8a6725f7fb04f68b768ad19ef91f36089b
mpiesiewicz <michal.piesiewicz@gmail.com>
2022-03-31 12:18:26 +0200

    day 51 done

--- day51 - twitter/main.py
+++ day51 - twitter/main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-from credentials import chrome_driver_path, GMAIL_ADDRESS, GMAIL_PASS
+from credentials import chrome_driver_path, GMAIL_ADDRESS, TWITTER_PASSWORD, TWITTER_LOGIN
 
 PROMISED_DOWN = 20
 PROMISED_UP = 2
@@ -18,7 +18,8 @@ class InternetSpeedTwitterBot:
         self.promised_up = promised_up
         self.chrome_driver_path = chrome_driver_path
         self.gmail_address = GMAIL_ADDRESS
-        self.gmail_pass = GMAIL_PASS
+        self.twitter_password = TWITTER_PASSWORD
+        self.twitter_login = TWITTER_LOGIN
         self.driver = self.initialize_driver()
 
     @staticmethod
@@ -50,11 +51,35 @@ class InternetSpeedTwitterBot:
         print(f'U: {upload_speed}')
         return download_speed, upload_speed
 
-    def tweet_to_internet_provider(self, download_speed, upload_speed):
-        self.driver.execute_script("window.open('https://www.twitter.com')")
-        login_google
+    def tweet_to_internet_provider(self, download_speed):
+        # self.driver.execute_script("window.open('https://www.twitter.com')")
+        self.driver.get('https://www.twitter.com')
+        time.sleep(3)
+        accept_cookies = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div[2]/div[1]/div/span/span')
+        accept_cookies.click()
+        log_in_btn = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/div[1]/div/div[3]/div[5]/a/div/span/span')
+        log_in_btn.click()
+        time.sleep(3)
+        username_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[5]/label/div/div[2]/div/input')
+        username_input.send_keys(self.twitter_login)
+        username_input.send_keys(Keys.ENTER)
+        time.sleep(3)
+        password_input = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        password_input.send_keys(self.twitter_password)
+        password_input.send_keys(Keys.ENTER)
+
+        time.sleep(3)
+
+        message = f'Orange, dlaczego prędkość mojego łącza to {download_speed} Mb/s jeśli płacę za 80 Mb/s?'
+
+        tweet_input = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
+        tweet_input.send_keys(message)
+        tweet_btn = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span')
+        tweet_btn.click()
+
 
 if __name__ == '__main__':
     bot = InternetSpeedTwitterBot(PROMISED_DOWN, PROMISED_UP)
-    # download_speed, upload_speed = bot.get_internet_speed()
-    bot.tweet_to_internet_provider(20, 7)
+    download_speed, upload_speed = bot.get_internet_speed()
+    time.sleep(2)
+    bot.tweet_to_internet_provider(20)
